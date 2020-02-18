0301200e1c1b668a204dd3816b3acf09cc06f7b1
Gustavo <graf.sousa@gmail.com>
2020-02-17 22:49:25 -0300

    [feature]: Add Background component

--- src/pages/SignIn/index.js
+++ src/pages/SignIn/index.js
@@ -1,10 +1,16 @@
 import React from 'react';
 import { Text } from 'react-native';
 
+import { Background } from '~/components/Background';
+
 // import { Container } from './styles';
 
 function SignIn() {
-  return <Text>SignIn</Text>;
+  return (
+    <Background>
+      <Text>SignIn</Text>
+    </Background>
+  );
 }
 
 export { SignIn };
