aeb308909166ae34da5b6502f04dd6a269b54358
Gustavo <graf.sousa@gmail.com>
2020-02-18 21:48:43 -0300

    [feature]: Add StatusBar

--- src/index.js
+++ src/index.js
@@ -1,10 +1,17 @@
 import 'react-native-gesture-handler';
+
 import React from 'react';
+import { StatusBar } from 'react-native';
 
 import { Routes } from './routes';
 
 function App() {
-  return <Routes />;
+  return (
+    <>
+      <StatusBar barStyle="light-content" backgroundColor="#7159c1" />
+      <Routes />
+    </>
+  );
 }
 
 export { App };
